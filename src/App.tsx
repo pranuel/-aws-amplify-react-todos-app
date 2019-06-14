e5df939dcf6dff6f073b63b5310cea4c794567f0
Manuel Pras <manuel.pras@deutschebahn.com>
2019-06-14 15:05:15 +0200

    add graphql api and use first query and mutation

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,41 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Amplify from 'aws-amplify';
 import awsconfig from './aws-exports';
 import { withAuthenticator } from 'aws-amplify-react'; // or 'aws-amplify-react-native';
+import Amplify, { API, graphqlOperation } from 'aws-amplify';
+import { CreateTodoInput } from './API';
+import { createTodo } from './graphql/mutations';
+import { listTodos } from './graphql/queries';
 
 Amplify.configure(awsconfig);
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  todoMutation = async () => {
+    const todoDetails: CreateTodoInput = {
+      name: 'Party tonight!',
+      description: 'Amplify CLI rocks!'
+    };
+
+    const newTodo = await API.graphql(graphqlOperation(createTodo, { input: todoDetails }));
+    alert(JSON.stringify(newTodo));
+  }
+
+  listQuery = async () => {
+    console.log('listing todos');
+    const allTodos = await API.graphql(graphqlOperation(listTodos));
+    alert(JSON.stringify(allTodos));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <button onClick={this.listQuery}>GraphQL Query</button>
+        <button onClick={this.todoMutation}>GraphQL Mutation</button>
+      </div>
+    );
+  }
 }
 
 export default withAuthenticator(App, true);
