060c9dc1ce970199a13b24f218a5b5ecfb5ad371
Manuel Pras <manuel.pras@deutschebahn.com>
2019-07-09 12:23:55 +0200

    fix tab size

--- src/ui-components/AddTodoForm.tsx
+++ src/ui-components/AddTodoForm.tsx
@@ -1,31 +1,31 @@
 import React from 'react';
 import { Form, Button, Icon, FormProps } from "semantic-ui-react";
 
-export interface AddTodoFormProps { 
-    onSubmit: (event: React.FormEvent<HTMLFormElement>, data: FormProps) => void,
-    todoName: string,
-    onChangeTodoName: (event: React.ChangeEvent<HTMLInputElement>) => void,
-    todoDescription?: string,
-    onChangeTodoDescription: (event: React.ChangeEvent<HTMLInputElement>) => void
+export interface AddTodoFormProps {
+  onSubmit: (event: React.FormEvent<HTMLFormElement>, data: FormProps) => void,
+  todoName: string,
+  onChangeTodoName: (event: React.ChangeEvent<HTMLInputElement>) => void,
+  todoDescription?: string,
+  onChangeTodoDescription: (event: React.ChangeEvent<HTMLInputElement>) => void
 }
 
 export function AddTodoForm(props: AddTodoFormProps) {
-    return (
-        <div>
-            <Form onSubmit={props.onSubmit}>
-                <Form.Field>
-                    <label>Name</label>
-                    <input placeholder='Name' type="text" value={props.todoName} onChange={props.onChangeTodoName} />
-                </Form.Field>
-                <Form.Field>
-                    <label>Description (optional)</label>
-                    <input placeholder='Description (optional)' type="text" value={props.todoDescription || ''} onChange={props.onChangeTodoDescription} />
-                </Form.Field>
-                <Button icon labelPosition='left' type='submit'>
-                    <Icon name='add' />
-                    Add Todo
+  return (
+    <div>
+      <Form onSubmit={props.onSubmit}>
+        <Form.Field>
+          <label>Name</label>
+          <input placeholder='Name' type="text" value={props.todoName} onChange={props.onChangeTodoName} />
+        </Form.Field>
+        <Form.Field>
+          <label>Description (optional)</label>
+          <input placeholder='Description (optional)' type="text" value={props.todoDescription || ''} onChange={props.onChangeTodoDescription} />
+        </Form.Field>
+        <Button icon labelPosition='left' type='submit'>
+          <Icon name='add' />
+          Add Todo
               </Button>
-            </Form>
-        </div>
-    );
+      </Form>
+    </div>
+  );
 }
--- src/ui-components/TodosList.tsx
+++ src/ui-components/TodosList.tsx
@@ -3,33 +3,33 @@ import { Todo } from "../todo.model";
 import { Message, List } from "semantic-ui-react";
 
 export interface TodosListProps {
-    todos: Todo[]
+  todos: Todo[]
 }
 
 export function TodosList(props: TodosListProps) {
-    return (
-        <div>
-            {
-                props.todos.length < 1 ?
-                    (
-                        <Message>
-                            <Message.Header>No todos exist...</Message.Header>
-                        </Message>
-                    )
-                    :
-                    (
-                        <List celled>
-                            {props.todos.map(todo => (
-                                <List.Item>
-                                    <List.Content>
-                                        <List.Header>{todo.name}</List.Header>
-                                        {todo.description || '/'}
-                                    </List.Content>
-                                </List.Item>
-                            ))}
-                        </List>
-                    )
-            }
-        </div>
-    )
+  return (
+    <div>
+      {
+        props.todos.length < 1 ?
+          (
+            <Message>
+              <Message.Header>No todos exist...</Message.Header>
+            </Message>
+          )
+          :
+          (
+            <List celled>
+              {props.todos.map(todo => (
+                <List.Item>
+                  <List.Content>
+                    <List.Header>{todo.name}</List.Header>
+                    {todo.description || '/'}
+                  </List.Content>
+                </List.Item>
+              ))}
+            </List>
+          )
+      }
+    </div>
+  )
 }
