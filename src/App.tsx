2b40ce81fe05ddb5103dcdfd0e17c799fcd184e3
Manuel Pras <manuel.pras@deutschebahn.com>
2019-07-24 12:00:16 +0200

    add dummy footer

--- src/App.tsx
+++ src/App.tsx
@@ -175,6 +175,22 @@ class App extends React.Component<{}, AppState> {
             }
           </ul>
         </section>
+        <footer className="footer">
+          <span className="todo-count">
+            <strong>0</strong>
+            <span> </span>
+            <span>items</span>
+            <span> left</span>
+          </span>
+          <ul className="filters">
+            <li><a href="#/" className="selected">All</a></li>
+            <span> </span>
+            <li><a href="#/active" className="">Active</a></li>
+            <span> </span>
+            <li><a href="#/completed" className="">Completed</a></li>
+          </ul>
+          <button className="clear-completed">Clear completed</button>
+        </footer>
       </div>
     );
   }
