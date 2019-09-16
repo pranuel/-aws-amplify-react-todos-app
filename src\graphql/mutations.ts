f3cfa548fd07a6fa71cb6a410da6cf961323b11d
Manuel Pras <manuel.pras@deutschebahn.com>
2019-09-16 13:14:34 +0200

    init amplify on new machine (small changes)

---
+++ src\graphql/mutations.ts
@@ -0,0 +1,27 @@
+// tslint:disable
+// this is an auto generated file. This will be overwritten
+
+export const createTodo = `mutation CreateTodo($input: CreateTodoInput!) {
+  createTodo(input: $input) {
+    id
+    description
+    isDone
+  }
+}
+`;
+export const updateTodo = `mutation UpdateTodo($input: UpdateTodoInput!) {
+  updateTodo(input: $input) {
+    id
+    description
+    isDone
+  }
+}
+`;
+export const deleteTodo = `mutation DeleteTodo($input: DeleteTodoInput!) {
+  deleteTodo(input: $input) {
+    id
+    description
+    isDone
+  }
+}
+`;
---
+++ src\graphql/subscriptions.ts
@@ -0,0 +1,27 @@
+// tslint:disable
+// this is an auto generated file. This will be overwritten
+
+export const onCreateTodo = `subscription OnCreateTodo {
+  onCreateTodo {
+    id
+    description
+    isDone
+  }
+}
+`;
+export const onUpdateTodo = `subscription OnUpdateTodo {
+  onUpdateTodo {
+    id
+    description
+    isDone
+  }
+}
+`;
+export const onDeleteTodo = `subscription OnDeleteTodo {
+  onDeleteTodo {
+    id
+    description
+    isDone
+  }
+}
+`;
